d43734b20e71ff9956334edfed69408fbd71b5af
Prem Kumar Nallamothu <Owner@prems-mbp.gateway.pace.com>
2015-02-19 12:24:45 -0500

    added a test class

---
+++ Test.h
@@ -0,0 +1,13 @@
+//
+//  Test.h
+//  Git Test
+//
+//  Created by Prem Kumar Nallamothu on 2/19/15.
+//  Copyright (c) 2015 Prem Kumar Nallamothu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Test : NSObject
+
+@end
---
+++ Test.m
@@ -0,0 +1,13 @@
+//
+//  Test.m
+//  Git Test
+//
+//  Created by Prem Kumar Nallamothu on 2/19/15.
+//  Copyright (c) 2015 Prem Kumar Nallamothu. All rights reserved.
+//
+
+#import "Test.h"
+
+@implementation Test
+
+@end
